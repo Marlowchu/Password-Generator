a5dea62b4b0ca08226ed3dea17bfe22bd499a5bd
Chuck Marlow <marlowchu@gmail.com>
2021-11-08 19:56:14 -0500

    cleaned up code and more comments

--- Develop/script.js
+++ Develop/script.js
@@ -6,7 +6,7 @@ var capital
 var number
 var special
 
-
+// array of possible outcomes
 var lowerOption = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numberOption = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 var specialOption = ["!", "@", "#", "$", "%", "^", "&", "*", "-", "+"]
@@ -24,58 +24,77 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+
+// when botton is clicked run create password function
 function writePassword (params) {
 
-  window.checkLenght();
+  createPassword();
 
 }
 
 
+// create password function
+var createPassword = function() {
 
-var checkLenght = function() {
-
-  // Ask user for their choice
+  // Ask user for lenth of password
   do {
 
-    length = window.prompt("Please enter valid passwerd length (8-128 chafacters)");
-    // var length = window.prompt("Length of password between 8-128 characters?");
-   
+    length = window.prompt("Please enter a passwerd length from 8-128)");
+
+    // If cancel buttom is pressed then exit function  
     if (length === null) {
       return;
     }
 
-
   }
-
+// check to make sure a number between 8-128 was entered. will continue to ask untill correct value is entered
   while (128 < length || length < 8 ||length === null || isNaN (length));
 
 
-  lower = confirm ("lowercase Characters? ('OK' FOR YES OR 'CANCEL' FOR NO)");
+  // ask user what type of characters they want in there password
+  for ( var i = 0; i < 1;) {
 
-  capital = confirm ("Capital Characters? ('OK' FOR YES OR 'CANCEL' FOR NO)");
+  lower = confirm ("Lowercase Characters? ('OK' FOR YES OR 'CANCEL' FOR NO)");
+
+  capital = confirm ("Uppercase Characters? ('OK' FOR YES OR 'CANCEL' FOR NO)");
 
   number = confirm ("Numbers? ('OK' FOR YES OR 'CANCEL' FOR NO)");
 
   special = confirm ("Special Characters? ('OK' FOR YES OR 'CANCEL' FOR NO)");
 
-  // generate password
+  // if user did not select any criteria generate a message
+  if (lower == false && capital == false && number == false && special == false) {
+    window.alert ( "Please select at least (1) character type"); 
+   }
 
-  // Math.random()
-  // lowerOption.length
+  // exist loop when at least 1 is chosen
+  if (lower == true || capital == true || number == true || special == true) {
+     i = 1
+   }
+  }
 
+  // generate password based on input from user
   var newChoice = "";  
 
 for ( var i = 0; i < length;) {
   
   
-  
+  // if user selected lowercase characters run if statement
   if (lower) {
+
+    // create random nuber between 0 and the length of lowercase array
     var index = Math.floor(Math.random() * lowerOption.length);
+
+    // the number generated will be the letter selected in the array
     var Choice = lowerOption[index];
+
+    // increment the counter by 1 untill you reach the length the user input
     i++;
-    // window.alert (Choice);
+    
+    // add charater to the password that is being built
     newChoice = newChoice + Choice;
 
+    // if you have reached the desired lenght of the password exit loop
     if (i==length){
       break
     }
@@ -87,12 +106,12 @@ for ( var i = 0; i < length;) {
   var index = Math.floor(Math.random() * numberOption.length);
   var Choice = numberOption[index];
   i++;
-  // window.alert (Choice);
+  
   newChoice = newChoice + Choice;
 
   if (i==length){
     break
-  }
+    }
   }
 
   if (special) {
@@ -100,11 +119,11 @@ for ( var i = 0; i < length;) {
   var index = Math.floor(Math.random() * specialOption.length);
   var Choice = specialOption[index];
   i++;
-  // window.alert (Choice);
+  
   newChoice = newChoice + Choice;
   if (i==length){
     break
-  }
+    }
   }
 
   if (capital) {
@@ -112,50 +131,27 @@ for ( var i = 0; i < length;) {
   var index = Math.floor(Math.random() * lowerOption.length);
   var Choice = lowerOption[index].toUpperCase();
   i++;
-  // window.alert (Choice);
+  
   newChoice = newChoice + Choice;
   if (i==length){
     break
-  }
+    }
   }
   
-  if (lower == false && capital == false && number == false && special == false) {
-   newChoice = "You create an invisable password";
-    i = length
-  }
+  
 }
-
+  
+// after running for loop display what was create in the loop
   var newChoice =  window.alert(newChoice)
 }
 
 
-    // lowercase prompt and verify
-
-  // do {
-
-  //   lowerCase = window.prompt("lowercase Characters? (YES OR NO)");
-
-  //   lowerCase = confirm ("lowercase Characters? ('OK' FOR YES OR 'CANCEL' FOR NO)")
-   
-  //   var length = window.prompt("Length of password between 8-128 characters?");
-   
-  //   if (lowerCase === null) {
-  //     return;
-  //   }
-
-  // }
-
-  // while (lowerCase != Boolean );
 
 
 
 
 
-// var upperCase = window.prompt("Uppercase Characters? (YES OR NO)");
 
-// var specialCharacter = window.prompt("Numeric characters? (YES OR NO)");
-
-// var specialCharacter = window.prompt("Special characters? (YES OR NO)");
 
 
 // GIVEN I need a new, secure password
